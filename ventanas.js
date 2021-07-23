5ed4ee89afa095506a26941392a9d02a3f95df5b
Pjsanchezh <pjsanchezh@gmail.com>
2021-07-22 22:19:32 -0400

    Implementation Windows

---
+++ ventanas.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//alertas dentro del navegador
+alert('mensaje de alerta');
+
+//ventanas de confirmacion
+var result = confirm('cuidado que vas a matar tu pc jiji');
+alert(result);
+
+//ventana de ingreso de datos
+
+var res = prompt('Realmente quieres matar tu pc', 'Me doy cuenta que si');
+alert('Dijiste que ' + res);
