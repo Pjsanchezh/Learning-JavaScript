c5df6fb4fd01df8580262305559820002d0f39c2
Pjsanchezh <pjsanchezh@gmail.com>
2021-07-22 22:04:38 -0400

    Implementation DoWhile

--- dowhile.js
+++ dowhile.js
@@ -4,5 +4,8 @@ var dato = 1;
 
 do{
     console.log( 'el valor de dato es ' + dato);
-    dato ++;
-}while(dato <=5);
+    dato ++; //IMPORTANTE
+
+}while(dato <1);
+
+console.log('salimos del bucle, yey');
