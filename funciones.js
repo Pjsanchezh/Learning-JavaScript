506d9dc35d5751620fd95c53712f30bf30b051d2
Pjsanchezh <pjsanchezh@gmail.com>
2021-07-24 19:57:23 -0400

    Functions

---
+++ funciones.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function NombreFuncion(){
+return 'Hola, return saludando desde la primera funcion';
+}
+
+function MostrarMensaje(){
+    console.log('Hola chaval, toy dentro de una funcion');
+}
+
+MostrarMensaje();
