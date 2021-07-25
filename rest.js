0b5265863bb5374e04fc37a6717312d8ebe0d5f1
Pjsanchezh <pjsanchezh@gmail.com>
2021-07-24 20:26:01 -0400

    Parameter REST

---
+++ rest.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Parametros REST
+
+function ListadoPaises(p1,p2, ...p){
+    console.log(p1);
+    console.log(p2);
+    console.log(p);
+
+
+}
+
+
+ListadoPaises('Chile', 'Peru', 'Bolivia', 'Argentina');
