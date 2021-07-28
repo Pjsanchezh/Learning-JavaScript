75fd370a902a3c83cc1011937dd708a7c2c91cc8
pjsanchezh <pjsanchezh@gmail.com>
2021-07-28 18:35:10 -0400

    Arrow Function

---
+++ flecha.js
@@ -0,0 +1,16 @@
+'use strict';
+ 
+//Funciones anonimas.
+
+function sumar (n1, n2, fun){
+    var suma = n1 + n2;
+    fun(suma);
+    return suma;
+};
+
+sumar(5,7, d => {
+    console.log('La suma es ' + d);
+    console.log('La multiplicacion es ' +  d * 2);
+});
+
+//Si lleva mas de un paramewtro si incluiremos los parentesis
